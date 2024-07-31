d43583c748d0d3cae19c68b707a5b007b1bd58d0
Austen Turner <austenpturner@msn.com>
2024-07-31 11:34:00 -0700

    use local storage in cart slice

--- src/store/slices/cart-slice.js
+++ src/store/slices/cart-slice.js
@@ -1,8 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const items =
+  localStorage.getItem("cartItems") !== null
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [];
+
+const total =
+  localStorage.getItem("cartTotal") !== null
+    ? JSON.parse(localStorage.getItem("cartTotal"))
+    : 0;
+
 const initialState = {
-  cartItems: [],
-  cartTotal: 0,
+  cartItems: items,
+  cartTotal: total,
 };
 
 const cartSlice = createSlice({
@@ -11,26 +21,43 @@ const cartSlice = createSlice({
   reducers: {
     addToCart(state, action) {
       state.cartItems.push(action.payload);
+      localStorage.setItem(
+        "cartItems",
+        JSON.stringify(state.cartItems.map((item) => item))
+      );
     },
     removeFromCart(state, action) {
       state.cartItems.splice(action.payload, 1);
+      localStorage.setItem(
+        "cartItems",
+        JSON.stringify(state.cartItems.map((item) => item))
+      );
     },
     increaseCartQuantity(state, action) {
       state.cartItems.splice(action.payload, 1, {
         ...state.cartItems[action.payload],
         quantity: state.cartItems[action.payload].quantity + 1,
       });
+      localStorage.setItem(
+        "cartItems",
+        JSON.stringify(state.cartItems.map((item) => item))
+      );
     },
     decreaseCartQuantity(state, action) {
       state.cartItems.splice(action.payload, 1, {
         ...state.cartItems[action.payload],
         quantity: state.cartItems[action.payload].quantity - 1,
       });
+      localStorage.setItem(
+        "cartItems",
+        JSON.stringify(state.cartItems.map((item) => item))
+      );
     },
     updateCartTotal(state, action) {
       state.cartTotal = parseFloat(
         (state.cartTotal + action.payload).toFixed(2)
       );
+      localStorage.setItem("cartTotal", JSON.stringify(state.cartTotal));
     },
   },
 });
