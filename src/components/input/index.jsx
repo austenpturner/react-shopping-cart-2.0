07fd0b4e5c1984f766a0b63d2ee446a863e8c0ae
Austen Turner <austenpturner@msn.com>
2024-08-01 11:17:06 -0700

    create common input and select components

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,3 +1,39 @@
-export default function Input() {
-  return <div>input</div>;
+import styles from "input.module.css";
+import PropTypes from "prop-types";
+
+export default function Input({
+  label,
+  name,
+  id,
+  type,
+  placeholder,
+  value,
+  onChange,
+  className,
+}) {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input
+        name={name}
+        id={id}
+        type={type || "text"}
+        placeholder={placeholder || "Enter value here... "}
+        value={value}
+        onChange={onChange}
+        className={className || styles.commonForm}
+      />
+    </>
+  );
 }
+
+Input.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  className: PropTypes.string,
+};
