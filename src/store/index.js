0f89b3e2cb3def538647850e0c437bec4ef554c4
Austen Turner <austenpturner@msn.com>
2024-08-21 16:43:49 -0700

    add cart slice middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./slices/cart-slice";
-import usersReducer from "./slices/users-slice";
+import usersReducer from "./slices/usersSlice";
+import cartReducer from "./slices/cartSlice";
+import { cartMiddleware } from "../middleware/cartMiddleware";
 
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
     users: usersReducer,
+    cart: cartReducer,
   },
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare().concat(cartMiddleware),
 });
 
 export default store;
