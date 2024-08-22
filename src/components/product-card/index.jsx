95cc3a7965ca885b7e0d137cd3048f5d8af5b61f
Austen Turner <austenpturner@msn.com>
2024-08-21 17:17:57 -0700

    update product card

--- src/components/product-card/index.jsx
+++ src/components/product-card/index.jsx
@@ -1,57 +1,46 @@
 import styles from "./product-card.module.css";
 import PropTypes from "prop-types";
-import CartBtnComponent from "../buttons/cart-btn";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addToCart,
-  increaseCartQuantity,
-  updateCartTotal,
-} from "../../store/slices/cart-slice";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import { useNavigate } from "react-router-dom";
-
+// import { LazyLoadImage } from "react-lazy-load-image-component";
+import Button from "../button";
+import { useContext } from "react";
+import { UIContext } from "../../context";
+import useCartActions from "../../hooks/useCartActions";
 export default function ProductCard({ product }) {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { cartItems } = useSelector((state) => state.cart);
-
-  function handleAddToCart() {
-    const index = cartItems.findIndex(
-      (cartItem) => cartItem.details.id === product.id
-    );
-    if (index !== -1) {
-      dispatch(increaseCartQuantity(index));
-      dispatch(updateCartTotal(product.price));
-    } else {
-      const item = {
-        details: product,
-        quantity: 1,
-      };
-      dispatch(addToCart(item));
-      dispatch(updateCartTotal(product.price));
-    }
-  }
+  const { state } = useContext(UIContext);
+  const buttonText = state.buttonText[product.id] || "Add to cart";
+  const { handleAddToCart } = useCartActions();
 
   return (
-    <div className={styles.productCard}>
-      <LazyLoadImage
-        src={product.thumbnail}
-        alt={product.title}
-        className={styles.productCardImg}
-        width={200}
-        height={200}
-        effect="blur"
-        onClick={() => navigate(`/product-details/${product.id}`)}
+    <li className={styles.productCard}>
+      <p className={styles.name}>{product.name}</p>
+      <p className={styles.price}>{`$${product.price}`}</p>
+      <Button
+        handleAction={handleAddToCart}
+        item={product}
+        text={buttonText}
+        type={"addToCart"}
       />
-      <p className={styles.title}>{product.title}</p>
-      <div>
-        <p className={styles.price}>{`$${product.price}`}</p>
-        <CartBtnComponent type={"add"} onClick={handleAddToCart} />
-      </div>
-    </div>
+    </li>
+    // <div className={styles.productCard}>
+    //   <LazyLoadImage
+    //     src={product.thumbnail}
+    //     alt={product.title}
+    //     className={styles.productCardImg}
+    //     width={200}
+    //     height={200}
+    //     effect="blur"
+    //     onClick={() => navigate(`/product-details/${product.id}`)}
+    //   />
+    //   <p className={styles.title}>{product.title}</p>
+    //   <div>
+    //     <p className={styles.price}>{`$${product.price}`}</p>
+    //     <CartBtnComponent type={"add"} onClick={handleAddToCart} />
+    //   </div>
+    // </div>
   );
 }
 
 ProductCard.propTypes = {
   product: PropTypes.object,
+  handleAddToCart: PropTypes.func,
 };
