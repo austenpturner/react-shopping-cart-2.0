ba0e1664a0f59e49050738668bcb18ae74c4560e
Austen Turner <austenpturner@msn.com>
2024-08-26 12:33:10 -0700

    add mobile nav menu

--- src/components/mainNav/index.jsx
+++ src/components/mainNav/index.jsx
@@ -4,46 +4,105 @@ import { useSelector } from "react-redux";
 import useAuth from "../../hooks/useAuth";
 import useLogout from "../../hooks/useLogout.js";
 import Button from "../button/index.jsx";
+import { useState } from "react";
 
 export default function MainNav() {
   const loading = useAuth();
   const navigate = useNavigate();
   const user = useSelector((state) => state.users.currentUser);
   const handleLogout = useLogout();
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  function handleToggleMobileNav() {
+    setMobileNavOpen(!mobileNavOpen);
+  }
 
   function handleClick() {
     if (user && confirm("Are you sure you want to log out?")) {
-      // console.log("log out requested");
       handleLogout();
+      handleToggleMobileNav();
     } else {
+      handleToggleMobileNav();
       navigate("/login");
     }
   }
 
   return (
-    <nav className={styles.mainNav}>
-      <ul className={styles.navLinkList}>
-        <li>
-          <Link to={"/"}>Products</Link>
-        </li>
-        <li>
-          <Link to={"cart"}>Cart</Link>
-        </li>
-        <li>
-          <Link to={"account"}>Account</Link>
-        </li>
-        <li>
-          {loading ? (
-            <button></button>
-          ) : (
-            <Button
-              handleAction={handleClick}
-              text={user ? "Logout" : "Log in"}
-              type={user ? "logout" : "login"}
-            />
-          )}
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.mainNavContainer}>
+      <button
+        aria-label={
+          mobileNavOpen ? "close navigation menu" : "open navigation menu"
+        }
+        aria-expanded={mobileNavOpen}
+        aria-controls="mainNav"
+        className={
+          mobileNavOpen
+            ? `${styles.mobileNavBtn} ${styles.toggleOpen}`
+            : `${styles.mobileNavBtn}`
+        }
+        onClick={handleToggleMobileNav}
+      >
+        <svg className={styles.hamburger} viewBox="0 0 100 100" width="35">
+          <rect
+            className={`${styles.line} ${styles.line__top}`}
+            width="90"
+            height="10"
+            x="5"
+            y="25"
+            rx="5"
+          ></rect>
+          <rect
+            className={`${styles.line} ${styles.line__middle}`}
+            width="90"
+            height="10"
+            x="5"
+            y="45"
+            rx="5"
+          ></rect>
+          <rect
+            className={`${styles.line} ${styles.line__bottom}`}
+            width="90"
+            height="10"
+            x="5"
+            y="65"
+            rx="5"
+          ></rect>
+        </svg>
+      </button>
+      <nav>
+        <ul
+          className={styles.mainNav}
+          id="mainNav"
+          data-visible={mobileNavOpen ? true : false}
+        >
+          <li>
+            <Link to={"/"} onClick={handleToggleMobileNav}>
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link to={"cart"} onClick={handleToggleMobileNav}>
+              Cart
+            </Link>
+          </li>
+          <li>
+            <Link to={"account"} onClick={handleToggleMobileNav}>
+              Account
+            </Link>
+          </li>
+          <li>
+            {loading ? (
+              <button></button>
+            ) : (
+              <Button
+                handleAction={handleClick}
+                text={user ? "Logout" : "Log in"}
+                type={user ? "logout" : "login"}
+              />
+            )}
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
