5a48fc408d251f1b21bebffab30a6b7055e6df5c
Austen Turner <austenpturner@msn.com>
2024-10-08 15:17:35 -0700

    update details page styles

--- src/components/productDetails/index.jsx
+++ src/components/productDetails/index.jsx
@@ -1,21 +1,16 @@
 import Button from "../button";
 import PropTypes from "prop-types";
 import styles from "./productDetails.module.scss";
-import { useContext, useState } from "react";
-import { FaCaretDown, FaCaretUp, FaStar } from "react-icons/fa";
+import { useContext } from "react";
+import { FaStar } from "react-icons/fa";
 import { UIContext } from "../../context/uiContext";
 import useCartActions from "../../hooks/useCartActions";
 import ProductImg from "../productImg/productImg";
 
 export default function ProductDetails({ product }) {
-  const [showReviews, setShowReviews] = useState(false);
   const { state } = useContext(UIContext);
   const buttonText = state.buttonText[product.id] || "Add to cart";
   const { handleAddToCart } = useCartActions();
-
-  function handleShowReviews() {
-    setShowReviews(!showReviews);
-  }
 
   return (
     <div className={styles.productContainer}>
@@ -24,9 +19,17 @@ export default function ProductDetails({ product }) {
         <div className={styles.details}>
           <div>
             <h1>{product?.title}</h1>
-            <p className={styles.description}>{product?.description}</p>
+            <div className={styles.ratingContainer}>
+              <span>
+                {[...Array(Math.round(product?.rating))].map((_, index) => {
+                  index += 1;
+                  return <FaStar key={index} />;
+                })}
+              </span>
+              <span>{`${product?.rating} out of 5`}</span>
+            </div>
           </div>
-
+          <p className={styles.description}>{product?.description}</p>
           <div className={styles.purchaseContainer}>
             <p className={styles.price}>{`$${product?.price}`}</p>
             <Button
@@ -38,56 +41,34 @@ export default function ProductDetails({ product }) {
           </div>
         </div>
       </div>
-      <hr />
-      <div className={styles.ratingsAndReviewsContainer}>
-        <h2>Reviews & Ratings</h2>
-        <div className={styles.ratingContainer}>
-          <div className={styles.column}>
-            <p>{`${product.reviews.length} reviews`}</p>
-            <Button
-              handleAction={handleShowReviews}
-              type="dropDown"
-              text="show reviews"
-              icon={showReviews ? <FaCaretUp /> : <FaCaretDown />}
-            />
-          </div>
-          <div className={styles.column}>
-            <p>{`${product?.rating} out of 5`}</p>
-            <div>
-              {[...Array(Math.round(product?.rating))].map((_, index) => {
-                index += 1;
-                return <FaStar key={index} />;
-              })}
-            </div>
-          </div>
-        </div>
-        <div className={styles.reviewsContainer}>
-          {showReviews ? (
-            <ul className={styles.reviewsList}>
-              {product?.reviews?.length > 0 ? (
-                product.reviews.map((review, index) => {
-                  return (
-                    <li key={index}>
-                      <p
-                        className={styles.reviewRating}
-                      >{`Rating: ${review.rating}`}</p>
-                      <p
-                        className={styles.reviewComment}
-                      >{`"${review.comment}"`}</p>
-                      <p
-                        className={styles.reviewName}
-                      >{`- ${review.reviewerName}`}</p>
-                    </li>
-                  );
-                })
-              ) : (
-                <p>No reviews.</p>
-              )}
-            </ul>
+      <div className={styles.reviewsContainer}>
+        <h2>Ratings & Reviews</h2>
+        <ul className={styles.reviewsList}>
+          {product?.reviews?.length > 0 ? (
+            product.reviews.map((review, index) => {
+              return (
+                <li key={index}>
+                  <span className={styles.reviewRating}>
+                    {[...Array(Math.round(review.rating))].map((_, index) => {
+                      index += 1;
+                      return <FaStar key={index} />;
+                    })}
+                  </span>
+                  <p>
+                    <span
+                      className={styles.reviewComment}
+                    >{`"${review.comment}"`}</span>
+                    <span
+                      className={styles.reviewName}
+                    >{`- ${review.reviewerName}`}</span>
+                  </p>
+                </li>
+              );
+            })
           ) : (
-            ""
+            <p>No reviews.</p>
           )}
-        </div>
+        </ul>
       </div>
     </div>
   );
