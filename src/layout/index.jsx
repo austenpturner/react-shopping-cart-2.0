d64b0f0bc34997254b99eec900e61465c6d5dcb6
Austen Turner <austenpturner@msn.com>
2024-10-16 15:56:37 -0700

    add scrollToTop button and functionality

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -3,6 +3,7 @@ import MainHeader from "../components/mainHeader";
 import Overlay from "../components/overlay";
 import Modal from "../components/modal";
 import Footer from "../components/footer";
+import ScrollToTopBtn from "../components/scrollBtn";
 
 export default function Layout() {
   return (
@@ -13,6 +14,7 @@ export default function Layout() {
         <Outlet />
         <Modal />
         <Footer />
+        <ScrollToTopBtn />
       </main>
     </>
   );
