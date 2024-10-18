c4a12b4da65f85c314aac9d2433b74b409418c3b
Austen Turner <austenpturner@msn.com>
2024-10-18 10:04:26 -0700

    update vite file and add 404.html in public folder

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/react-shopping-cart-2.0/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
