6b7aceddbfa7516f582ab3b47a1f9a1aa5d67bc5
Austen Turner <austenpturner@msn.com>
2024-10-18 10:49:38 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/react-shopping-cart-2.0/",
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  // build: {
+  //   outDir: "dist",
+  // },
+  // resolve: {
+  //   alias: {
+  //     "@": "/src",
+  //   },
+  // },
 });
