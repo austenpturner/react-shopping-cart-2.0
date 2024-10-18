a19cc3f4438c3f7da269d2890f4ab9c1ad3c7bc6
Austen Turner <austenpturner@msn.com>
2024-10-18 09:36:41 -0700

    update firebaseConfig and .env.production files

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,14 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "react-shopping-cart-2.firebaseapp.com",
   projectId: "react-shopping-cart-2",
   storageBucket: "react-shopping-cart-2.appspot.com",
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
