8659357f12d03e593eeaca2a107bf1e007da475f
Austen Turner <austenpturner@msn.com>
2024-10-18 11:19:19 -0700

    hide api keys

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -12,8 +12,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log(firebaseConfig);
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
