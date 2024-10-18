1abb8ff9d22072a333ed543a30bdeb369561b88a
Austen Turner <austenpturner@msn.com>
2024-10-18 10:32:44 -0700

    Updates

--- src/pages/placeholder/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./styles.scss";
-
-export default function PlaceholderPage() {
-  return (
-    <div className="page-container placeholder-page">
-      <h1>Coming soon... </h1>
-    </div>
-  );
-}
--- src/util/isStrongPassword.js
+++
@@ -1,4 +0,0 @@
-export function isStrongPassword(password) {
-  const passwordRegex = /^(?=.*?[a-z])(?=.*?[0-9]).{6,}$/;
-  return passwordRegex.test(password);
-}
